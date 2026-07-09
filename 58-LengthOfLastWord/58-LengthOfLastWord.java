dbb2d041faec6040152145fd576581d366824aad
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:06:37 +0530

    Create 58-LengthOfLastWord

---
+++ 58-LengthOfLastWord/58-LengthOfLastWord.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:06:38 PM
+class Solution {
+
+    public int lengthOfLastWord(String s) {
+
+        int len = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+}
