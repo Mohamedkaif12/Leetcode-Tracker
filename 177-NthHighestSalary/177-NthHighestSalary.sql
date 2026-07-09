08795b8f3e96051e1076e4cde8259fc8ccf5227a
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:05:38 +0530

    Create 177-NthHighestSalary

---
+++ 177-NthHighestSalary/177-NthHighestSalary.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/9/2026, 3:05:38 PM
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+    );
+END
