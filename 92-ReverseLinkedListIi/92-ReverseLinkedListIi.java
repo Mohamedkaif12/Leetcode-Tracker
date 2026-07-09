901f379738d4ad1d7e76f255d4e01f38e0d20646
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:05:59 +0530

    Create 92-ReverseLinkedListIi

---
+++ 92-ReverseLinkedListIi/92-ReverseLinkedListIi.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 3:06:00 PM
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if (head == null)
+            return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        for (int i = 1; i < left; i++)
+            prev = prev.next;
+
+        ListNode curr = prev.next;
+
+        for (int i = 0; i < right - left; i++) {
+
+            ListNode next = curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+
+        return dummy.next;
+    }
+}
