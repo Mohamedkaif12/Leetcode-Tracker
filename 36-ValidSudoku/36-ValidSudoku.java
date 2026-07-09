03871948036b19d6a43c4f03dae69b33bfcf6f0f
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:07:05 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 3:07:05 PM
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+
+                char num = board[i][j];
+
+                if (num == '.')
+                    continue;
+
+                if (!set.add(num + "row" + i) ||
+                    !set.add(num + "col" + j) ||
+                    !set.add(num + "box" + i/3 + "-" + j/3))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
