29f5a8c946519e1128231c09f06c071b1647bb3a
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:07:11 +0530

    Create 32-LongestValidParentheses

---
+++ 32-LongestValidParentheses/32-LongestValidParentheses.java
@@ -0,0 +1,39 @@
+// Last updated: 7/9/2026, 3:07:11 PM
+import java.util.*;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+
+                stack.push(i);
+
+            } else {
+
+                stack.pop();
+
+                if (stack.isEmpty()) {
+
+                    stack.push(i);
+
+                } else {
+
+                    maxLength = Math.max(
+                        maxLength,
+                        i - stack.peek()
+                    );
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
