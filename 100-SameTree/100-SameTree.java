828c649fc86a666ea8e7cdc09c9b51b863426b0b
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:05:51 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:05:51 PM
+class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        return isSameTree(p.left, q.left)
+            && isSameTree(p.right, q.right);
+    }
+}
