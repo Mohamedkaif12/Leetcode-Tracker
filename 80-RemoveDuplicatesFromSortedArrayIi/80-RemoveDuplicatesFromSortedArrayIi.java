28b6cdd9b9b6eadcdf675d878fe24c09980d12fb
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:06:18 +0530

    Create 80-RemoveDuplicatesFromSortedArrayIi

---
+++ 80-RemoveDuplicatesFromSortedArrayIi/80-RemoveDuplicatesFromSortedArrayIi.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:06:18 PM
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length <= 2)
+            return nums.length;
+
+        int index = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
