f9aecced0a804c4d8a634b55c1ec870f790b6ab4
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:06:14 +0530

    Create 82-RemoveDuplicatesFromSortedListIi

---
+++ 82-RemoveDuplicatesFromSortedListIi/82-RemoveDuplicatesFromSortedListIi.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 3:06:14 PM
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (head != null) {
+            while (head.next != null && head.val == head.next.val)
+                head = head.next;
+
+            if (prev.next == head)
+                prev = prev.next;
+            else
+                prev.next = head.next;
+
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
