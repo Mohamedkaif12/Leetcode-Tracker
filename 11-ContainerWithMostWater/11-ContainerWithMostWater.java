a356429dfe2f49c9bfe9e7a27dea293e15e1d59a
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:07:46 +0530

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:07:46 PM
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = 0;
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return max;
+    }
+}
