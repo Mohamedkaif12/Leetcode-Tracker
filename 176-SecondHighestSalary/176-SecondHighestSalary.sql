82ed1c524f38822130a7144555746567f7c6abbf
Mohamed anser kaif A <kaifanser1122@gmail.com>
2026-07-09 15:05:40 +0530

    Create 176-SecondHighestSalary

---
+++ 176-SecondHighestSalary/176-SecondHighestSalary.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/9/2026, 3:05:40 PM
+SELECT 
+     (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET 1
+
+     )AS SecondHighestSalary;
